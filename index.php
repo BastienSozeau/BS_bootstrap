15ac309da287ecb236ed18d1f8b0e8d5a636b858
BastienSozeau <bsozeau@BS-MacBookPro.local>
2020-01-06 10:54:36 +0100

    add index

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title>BS Bootstrap</title>
+	<style>
+	
+	img{
+		width: 	100%;
+		height: auto
+	}
+	.mon-image{
+		width: 300px;
+	}
+	
+		
+	</style>
+</head>
+<body>
+	<h1 class="big-title">BS Bootstrap</h1>
+		
+</body>
+
+</html>
