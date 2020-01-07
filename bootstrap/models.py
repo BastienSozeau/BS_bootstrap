9b9ae5bcc214088244f4d97701ca02a1bfa46646
BastienSozeau <bsozeau@BS-MacBookPro.local>
2020-01-07 10:25:39 +0100

    remove folder

--- bootstrap/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Project(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    text_content = models.CharField(max_length=2000)
-    image = models.FilePathField(path="/img")
