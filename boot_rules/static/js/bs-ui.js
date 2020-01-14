ba2563a094448c9a50f30227d2e31cd26e229a62
BastienSozeau <bastien.sozeau@gmail.com>
2020-01-14 09:51:05 +0100

    update

--- boot_rules/static/js/bs-ui.js
+++ boot_rules/static/js/bs-ui.js
@@ -1,9 +1,16 @@
 (function($) {
 	$(document).ready(function() {
 		
+// Alert
 		$('.bs--alert-close').on('click',function(e){
 			e.preventDefault();
 			$(this).parent().slideUp(200);
+		});		
+
+// Menu
+		$('.bs--menu-item').on('click',function(){
+			$(this).parent().children('.bs--menu-item').removeClass('active');
+			$(this).addClass('active');
 		});		
 		
 	});
